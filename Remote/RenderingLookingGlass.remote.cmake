2d9d860ad0656e9571a83a4e6616736cda13d8ab
SoldierDown <dyhard0520@gmail.com>
2024-01-15 00:55:39 -0800

    init

---
+++ Remote/RenderingLookingGlass.remote.cmake
@@ -0,0 +1,9 @@
+#
+# Looking Glass Remote Module
+#
+
+vtk_fetch_module(RenderingLookingGlass
+        "Looking Glass Support for VTK"
+        GIT_REPOSITORY https://github.com/Kitware/LookingGlassVTKModule
+        GIT_TAG a970d1bee245374eec8df25a5e3d1b45b6164c6c
+        )
---
+++ Remote/vtkDICOM.remote.cmake
@@ -0,0 +1,9 @@
+#
+# Dicom Classes
+#
+
+vtk_fetch_module(vtkDICOM
+  "Dicom classes and utilities"
+  GIT_REPOSITORY https://github.com/dgobbi/vtk-dicom
+  GIT_TAG bc8d11667ae3158a23a83523d588cab85ec6c3aa
+  )
